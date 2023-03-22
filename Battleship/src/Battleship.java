ccdb1b1afd9f751b6429a515a5b9a4d28eb0b067
Jacopo <jacopo.ceccuti@gmail.com>
2023-03-22 10:05:50 +0100

    WIth love

--- Battleship/src/Battleship.java
+++ Battleship/src/Battleship.java
@@ -4,7 +4,11 @@ public class Battleship {
 
     private static int playerTurn;
     static Scanner scanner = new Scanner(System.in);
+    private static boolean singlePlayerMode;
 
+    private static Player p1;
+    private static Player p2;
+    
     public static void main(String[] args) {
 
         Battleship game = new Battleship();
@@ -13,17 +17,41 @@ public class Battleship {
         Grid grid2 = new Grid();
 
         
-        // TODO: Add scanner to get player name
-        Player p1 = new Player("Player 1", grid1, grid2);
-        p1.placeShips();
+        System.out.println("Would you like to play agianst the computer? (y/n)");
+        boolean singlePlayerModeSet = false;
+        do {
+            char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
+            if (resp == 'y') {
+            	singlePlayerMode = true;
+            	singlePlayerModeSet = true;
+            } else if (resp == 'n') {
+            	singlePlayerMode = false;
+            	singlePlayerModeSet = true;
+            } else {
+                System.out.println("Please only type 'y' or 'n' ");
+            }
+        } while (singlePlayerModeSet = false);
 
-        AI p2 = new AI("CPU", grid2, grid1);
-        //Player p2 = new Player("Player 2", grid2, grid1);
-        p2.placeShips();
-
+        if (singlePlayerMode) {
+        	p1 = new Player("Player 1", grid1, grid2);
+        	p1.placeShips();
+        	p2 = new AI("CPU", grid2, grid1);
+        	p2.placeShips();
+        	
+        } else {
+        	 p1 = new Player("Player 1", grid1, grid2);
+             p1.placeShips();
+             p2 = new Player("Player 2", grid2, grid1);
+             p2.placeShips();
+        }
+        
+     // TODO: Add scanner to get player name
+        
+        
         playerTurn = 1;
         while (true) {
-            if (playerTurn == 1) {
+            
+			if (playerTurn == 1) {
                 p1.shoot();
                 if (p2.areAllShipsSunk()) {
                     game.endGame(p1);
