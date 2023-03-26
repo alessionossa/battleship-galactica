a16aeba89f7c3f2dd36306d617db141dc89f4f67
Oliwia Pindel <s215142@dtu.dk>
2023-03-26 19:25:22 +0200

    Allow for realistic placing and removing ships and extract command line inputs to separate functions

--- Battleship/Model/src/main/java/com/galactica/model/Ship.java
+++ Battleship/Model/src/main/java/com/galactica/model/Ship.java
@@ -7,14 +7,14 @@ public class Ship {
     }
 
     private int length;
-    private boolean sunk;  //Status of the ship, true if ship is sunk, false if not
+    private boolean sunk; // Status of the ship, true if ship is sunk, false if not
     private ShipType shipType;
     private int identifier;
 
     Coordinate coordinate;
     Direction direction;
 
-    public Ship(int length, ShipType shipType, int identifier){
+    public Ship(int length, ShipType shipType, int identifier) {
         this.length = length;
         this.shipType = shipType;
         this.identifier = identifier;
@@ -55,5 +55,12 @@ public class Ship {
 
     public boolean isSunk() {
         return sunk;
+    }
+
+    public boolean isPlaced() {
+        if (coordinate != null)
+            return true;
+        else
+            return false;
     }
 }
