f285a0cb3b47e1ec5656e847974d9bb3d24479e7
Alessio Nossa <s215206@student.dtu.dk>
2023-03-29 15:23:15 +0200

    Change indentation to spaces

--- Battleship/Model/src/main/java/com/galactica/model/Battleship.java
+++ Battleship/Model/src/main/java/com/galactica/model/Battleship.java
@@ -4,97 +4,97 @@ import java.util.Scanner;
 
 public class Battleship {
 
-	private static int playerTurn;
-	private static boolean asteroidMode;
+    private static int playerTurn;
+    private static boolean asteroidMode;
 
-	static Scanner scanner = new Scanner(System.in);
-	private static boolean singlePlayerMode;
+    static Scanner scanner = new Scanner(System.in);
+    private static boolean singlePlayerMode;
 
-	private static Player p1;
-	private static Player p2;
+    private static Player p1;
+    private static Player p2;
 
-	public static boolean getAsteroidModeResponse() {
-		System.out.println("Would you like to play in obstacle mode? (y/n)");
-		while (true) {
-			char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
-			if (resp == 'y') {
-				return true;
-			} else if (resp == 'n') {
-				return false;
-			} else {
-				System.out.println("Please only type 'y' or 'n' ");
-			}
-		}
+    public static boolean getAsteroidModeResponse() {
+        System.out.println("Would you like to play in obstacle mode? (y/n)");
+        while (true) {
+            char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
+            if (resp == 'y') {
+                return true;
+            } else if (resp == 'n') {
+                return false;
+            } else {
+                System.out.println("Please only type 'y' or 'n' ");
+            }
+        }
 
-	}
+    }
 
-	public static boolean getPlayerModeResponse() {
-		System.out.println("Would you like to play against the computer? (y/n)");
-		while (true) {
-			char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
-			if (resp == 'y') {
-				return true;
-			} else if (resp == 'n') {
-				return false;
-			} else {
-				System.out.println("Please only type 'y' or 'n' ");
-			}
-		}
+    public static boolean getPlayerModeResponse() {
+        System.out.println("Would you like to play against the computer? (y/n)");
+        while (true) {
+            char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
+            if (resp == 'y') {
+                return true;
+            } else if (resp == 'n') {
+                return false;
+            } else {
+                System.out.println("Please only type 'y' or 'n' ");
+            }
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Battleship game = new Battleship();
+        Battleship game = new Battleship();
 
-		Grid grid1 = new Grid();
-		Grid grid2 = new Grid();
+        Grid grid1 = new Grid();
+        Grid grid2 = new Grid();
 
-		asteroidMode = getAsteroidModeResponse();
-		if (asteroidMode) {
-			grid1.placeAsteroids();
-			grid2.placeAsteroids();
-		}
+        asteroidMode = getAsteroidModeResponse();
+        if (asteroidMode) {
+            grid1.placeAsteroids();
+            grid2.placeAsteroids();
+        }
 
-		singlePlayerMode = getPlayerModeResponse();
-		p1 = new Human(grid1, grid2);
-		p1.placeShips();
+        singlePlayerMode = getPlayerModeResponse();
+        p1 = new Human(grid1, grid2);
+        p1.placeShips();
 
-		if (singlePlayerMode) {
-			p2 = new AI("CPU", grid2, grid1);
+        if (singlePlayerMode) {
+            p2 = new AI("CPU", grid2, grid1);
 
-		} else {
-			p2 = new Human(grid2, grid1);
-		}
-		p2.placeShips();
+        } else {
+            p2 = new Human(grid2, grid1);
+        }
+        p2.placeShips();
 
-		playerTurn = 1;
-		while (true) {
+        playerTurn = 1;
+        while (true) {
 
-			if (playerTurn == 1) {
-				p1.shoot();
-				if (p2.areAllShipsSunk()) {
-					game.endGame(p1);
-					return;
-				}
+            if (playerTurn == 1) {
+                p1.shoot();
+                if (p2.areAllShipsSunk()) {
+                    game.endGame(p1);
+                    return;
+                }
 
-			} else {
-				p2.shoot();
-				if (p1.areAllShipsSunk()) {
-					game.endGame(p2);
-					return;
-				}
+            } else {
+                p2.shoot();
+                if (p1.areAllShipsSunk()) {
+                    game.endGame(p2);
+                    return;
+                }
 
-			}
-			if (playerTurn == 1)
-				playerTurn = 2;
-			else
-				playerTurn = 1;
-		}
-	}
+            }
+            if (playerTurn == 1)
+                playerTurn = 2;
+            else
+                playerTurn = 1;
+        }
+    }
 
-	void endGame(Player winner) {
-		System.out.println("Congratulations " + winner.getName() + "! 🍾🎉");
-	}
+    void endGame(Player winner) {
+        System.out.println("Congratulations " + winner.getName() + "! 🍾🎉");
+    }
 
 }
--- Battleship/Model/src/main/java/com/galactica/model/Human.java
+++ Battleship/Model/src/main/java/com/galactica/model/Human.java
@@ -2,158 +2,158 @@ package com.galactica.model;
 
 public class Human extends Player {
 
-	public Human(Grid ownGrid, Grid opponentGrid) {
-		super(ownGrid, opponentGrid);
-		System.out.println("Enter player name: ");
-		this.name = Player.sc.nextLine();
-	}
+    public Human(Grid ownGrid, Grid opponentGrid) {
+        super(ownGrid, opponentGrid);
+        System.out.println("Enter player name: ");
+        this.name = Player.sc.nextLine();
+    }
 
-	boolean getPlaceOrRemoveResponse() { // true = place, false = remove
-		if (ownGrid.anyShipsPlaced()) {
-			System.out
-					.println(name + ", would you like to place or remove a ship?: type 'p' for place, 'r' for remove");
-			while (true) {
-				char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
-				if (resp == 'p')
-					return true;
-				else if (resp == 'r')
-					return false;
-				else
-					System.out.println("Please only type 'p' or 'r'");
-			}
-		} else
-			return true;
-	}
+    boolean getPlaceOrRemoveResponse() { // true = place, false = remove
+        if (ownGrid.anyShipsPlaced()) {
+            System.out
+                    .println(name + ", would you like to place or remove a ship?: type 'p' for place, 'r' for remove");
+            while (true) {
+                char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
+                if (resp == 'p')
+                    return true;
+                else if (resp == 'r')
+                    return false;
+                else
+                    System.out.println("Please only type 'p' or 'r'");
+            }
+        } else
+            return true;
+    }
 
-	Ship selectShip() {
-		System.out.println(name + ", select a ship: 'd' for death, 'c' for cruiser, 's' for scout");
-		// to use more than one ship of each type could use d1, d2, d3 but won't matter
-		// with gui
-		while (true) {
-			char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
-			if (resp == 'd')
-				return ships[0];
-			else if (resp == 'c')
-				return ships[1];
-			else if (resp == 's')
-				return ships[2];
-			else
-				System.out.println("Please only type 'd', 'c' or 's'");
-		}
-	}
+    Ship selectShip() {
+        System.out.println(name + ", select a ship: 'd' for death, 'c' for cruiser, 's' for scout");
+        // to use more than one ship of each type could use d1, d2, d3 but won't matter
+        // with gui
+        while (true) {
+            char resp = Character.toLowerCase(Battleship.scanner.nextLine().charAt(0));
+            if (resp == 'd')
+                return ships[0];
+            else if (resp == 'c')
+                return ships[1];
+            else if (resp == 's')
+                return ships[2];
+            else
+                System.out.println("Please only type 'd', 'c' or 's'");
+        }
+    }
 
-	void placeShips() {
-		boolean allShipsPlaced = false;
-		while (!allShipsPlaced) {
-			ownGrid.printGrid(true);
-			boolean placeOrRemove = getPlaceOrRemoveResponse();
+    void placeShips() {
+        boolean allShipsPlaced = false;
+        while (!allShipsPlaced) {
+            ownGrid.printGrid(true);
+            boolean placeOrRemove = getPlaceOrRemoveResponse();
 
-			Ship ship = selectShip();
-			if (!placeOrRemove && !ship.isPlaced()) {
-				System.out.println("Cannot remove a ship that hasn't been placed");
-			} else if (placeOrRemove && ship.isPlaced()) {
-				System.out.println("Cannot place a ship that is already on the grid");
-			} else if (!placeOrRemove && ship.isPlaced()) {
-				removeShip(ship);
-			} else if (placeOrRemove && !ship.isPlaced()) {
-				placeShip(ship);
-			}
+            Ship ship = selectShip();
+            if (!placeOrRemove && !ship.isPlaced()) {
+                System.out.println("Cannot remove a ship that hasn't been placed");
+            } else if (placeOrRemove && ship.isPlaced()) {
+                System.out.println("Cannot place a ship that is already on the grid");
+            } else if (!placeOrRemove && ship.isPlaced()) {
+                removeShip(ship);
+            } else if (placeOrRemove && !ship.isPlaced()) {
+                placeShip(ship);
+            }
 
-			allShipsPlaced = hasAllShipsPlaced();
+            allShipsPlaced = hasAllShipsPlaced();
 
-		}
+        }
 
-	}
+    }
 
-	void placeShip(Ship ship) {
+    void placeShip(Ship ship) {
 
-		boolean isValidShipPosition;
-		do {
+        boolean isValidShipPosition;
+        do {
 
-			Coordinate coordinate;
-			boolean isValidCoordinate = false;
-			do {
-				System.out.println("\n" + name + ", where do you want to place the ship?");
-				System.out.println("Enter X-coordinate:");
-				char x0 = Battleship.scanner.nextLine().charAt(0);
-				System.out.println("Enter Y-coordinate:");
-				int y0 = Integer.parseInt(Battleship.scanner.nextLine());
+            Coordinate coordinate;
+            boolean isValidCoordinate = false;
+            do {
+                System.out.println("\n" + name + ", where do you want to place the ship?");
+                System.out.println("Enter X-coordinate:");
+                char x0 = Battleship.scanner.nextLine().charAt(0);
+                System.out.println("Enter Y-coordinate:");
+                int y0 = Integer.parseInt(Battleship.scanner.nextLine());
 
-				coordinate = new Coordinate(x0, y0);
-				isValidCoordinate = ownGrid.isValidCoordinate(coordinate);
+                coordinate = new Coordinate(x0, y0);
+                isValidCoordinate = ownGrid.isValidCoordinate(coordinate);
 
-				if (!isValidCoordinate) {
-					System.out.println("The coordinates you entered are not valid.");
-				}
-			} while (!isValidCoordinate);
+                if (!isValidCoordinate) {
+                    System.out.println("The coordinates you entered are not valid.");
+                }
+            } while (!isValidCoordinate);
 
-			Direction direction = null;
-			do {
-				System.out.println(name
-						+ ", which direction do you want to place the ship? Enter H for horizontal, V for vertical.");
-				char directionChar = Battleship.scanner.nextLine().toLowerCase().trim().charAt(0);
+            Direction direction = null;
+            do {
+                System.out.println(name
+                        + ", which direction do you want to place the ship? Enter H for horizontal, V for vertical.");
+                char directionChar = Battleship.scanner.nextLine().toLowerCase().trim().charAt(0);
 
-				if (directionChar == Direction.Horizontal.getCharIdentifier()
-						|| directionChar == Direction.Vertical.getCharIdentifier())
-					direction = Direction.get(directionChar);
-			} while (direction == null);
+                if (directionChar == Direction.Horizontal.getCharIdentifier()
+                        || directionChar == Direction.Vertical.getCharIdentifier())
+                    direction = Direction.get(directionChar);
+            } while (direction == null);
 
-			ship.setCoordinate(coordinate);
-			ship.setDirection(direction);
+            ship.setCoordinate(coordinate);
+            ship.setDirection(direction);
 
-			isValidShipPosition = ownGrid.isValidShipPosition(ship, coordinate, direction);
-			if (isValidShipPosition) {
-				ownGrid.placeShip(ship, coordinate, direction);
-			} else {
-				System.out.println("You cannot place a ship here.");
-			}
+            isValidShipPosition = ownGrid.isValidShipPosition(ship, coordinate, direction);
+            if (isValidShipPosition) {
+                ownGrid.placeShip(ship, coordinate, direction);
+            } else {
+                System.out.println("You cannot place a ship here.");
+            }
 
-		} while (!isValidShipPosition);
+        } while (!isValidShipPosition);
 
-	}
+    }
 
-	void removeShip(Ship ship) {
-		ownGrid.removeShip(ship);
-		ship.setCoordinate(null);
-		ship.setDirection(null);
-	}
+    void removeShip(Ship ship) {
+        ownGrid.removeShip(ship);
+        ship.setCoordinate(null);
+        ship.setDirection(null);
+    }
 
-	void shoot() {
-		Coordinate coordinate;
-		boolean isValidCoordinate;
-		do {
-			System.out.println("\n-----------------------------\nOpponents grid:");
-			opponentGrid.printGrid(false);
+    void shoot() {
+        Coordinate coordinate;
+        boolean isValidCoordinate;
+        do {
+            System.out.println("\n-----------------------------\nOpponents grid:");
+            opponentGrid.printGrid(false);
 
-			System.out.println(name + ", where do you want to shoot?");
-			System.out.println("Enter X-coordinate:");
-			char x0 = Battleship.scanner.nextLine().charAt(0);
-			System.out.println("Enter Y-coordinate:");
-			int y0 = Integer.parseInt(Battleship.scanner.nextLine());
+            System.out.println(name + ", where do you want to shoot?");
+            System.out.println("Enter X-coordinate:");
+            char x0 = Battleship.scanner.nextLine().charAt(0);
+            System.out.println("Enter Y-coordinate:");
+            int y0 = Integer.parseInt(Battleship.scanner.nextLine());
 
-			coordinate = new Coordinate(x0, y0);
-			isValidCoordinate = opponentGrid.isValidCoordinate(coordinate);
+            coordinate = new Coordinate(x0, y0);
+            isValidCoordinate = opponentGrid.isValidCoordinate(coordinate);
 
-			if (!isValidCoordinate) {
-				System.out.println("The coordinates you entered are not valid.");
-			}
-		} while (!isValidCoordinate);
+            if (!isValidCoordinate) {
+                System.out.println("The coordinates you entered are not valid.");
+            }
+        } while (!isValidCoordinate);
 
-		opponentGrid.setTile(coordinate, true);
+        opponentGrid.setTile(coordinate, true);
 
-		Asteroid asteroidAtCoordinate = opponentGrid.getAsteroidAtCoordinate(coordinate);
-		Ship shipAtCoordinate = opponentGrid.getShipAtCoordinate(coordinate);
-		if (shipAtCoordinate != null || asteroidAtCoordinate != null) {
-			if (shipAtCoordinate != null) {
-				boolean isShipSunk = opponentGrid.checkIfShipIsSunk(shipAtCoordinate);
-				if (isShipSunk) {
-					shipAtCoordinate.setSunk(true);
-					System.out.println("You sunk a ship! 💥🚢");
-				}
-			} else
-				System.out.println("You hit something!");
-		} else
-			System.out.println("You missed");
+        Asteroid asteroidAtCoordinate = opponentGrid.getAsteroidAtCoordinate(coordinate);
+        Ship shipAtCoordinate = opponentGrid.getShipAtCoordinate(coordinate);
+        if (shipAtCoordinate != null || asteroidAtCoordinate != null) {
+            if (shipAtCoordinate != null) {
+                boolean isShipSunk = opponentGrid.checkIfShipIsSunk(shipAtCoordinate);
+                if (isShipSunk) {
+                    shipAtCoordinate.setSunk(true);
+                    System.out.println("You sunk a ship! 💥🚢");
+                }
+            } else
+                System.out.println("You hit something!");
+        } else
+            System.out.println("You missed");
 
-	}
+    }
 }
