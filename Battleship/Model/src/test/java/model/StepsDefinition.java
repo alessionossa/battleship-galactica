15e77eaeefd487b463dcd15a71ed1a6028817fcb
Oliwia Pindel <s215142@dtu.dk>
2023-03-29 17:20:10 +0200

    First cucumber test

--- Battleship/Model/src/test/java/model/StepsDefinition.java
+++ Battleship/Model/src/test/java/model/StepsDefinition.java
@@ -1,32 +1,50 @@
 package model;
+
+import static org.junit.Assert.assertEquals;
+
+import com.galactica.model.Coordinate;
+import com.galactica.model.Direction;
+import com.galactica.model.Grid;
+import com.galactica.model.Human;
+import com.galactica.model.Player;
+import com.galactica.model.Ship;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepsDefinition {
-	@Given("The game has not started")
-	public void the_game_has_not_started() {
 
-	}
-
-	@Given("I have an empty grid")
-	public void i_have_an_empty_grid() {
-	}
+	Grid ownGrid;
+	Grid opponentGrid;
+	Player player;
+	Ship ship;
 
 	@Given("Not all my ships have been placed")
 	public void not_all_my_ships_have_been_placed() {
-	} 
+		ownGrid = new Grid();
+		opponentGrid = new Grid();
+		player = new Human(ownGrid, opponentGrid);
 
-	@When("I place a ship of length {int} {string} on tile {string}")
-	public void i_place_a_ship_of_length_on_tile(Integer int1, String string, String string2) {
 	}
 
-	@Then("The ship appears on tiles {string}, {string} and {string}")
-	public void the_ship_appears_on_tiles_and(String string, String string2, String string3) {
+	@When("I place a {Ship} in direction {char} on coordinate {char} {int}")
+	public void i_place_a_ship_in_direction_on_coordinate(Ship shipInput, char directionChar, char x, int y) {
+		ship = shipInput;
+		Coordinate coordinate = new Coordinate(x, y);
+		Direction direction = Direction.get(directionChar);
+		player.placeShip(ship, coordinate, direction);
 	}
 
-	@Then("I have one less ship to be placed")
-	public void i_have_one_less_ship_to_be_placed() {
+	@Then("The ship is placed on tiles {char} {int}, {char} {int} and {char} {int}")
+	public void the_ship_is_placed_on_tiles_and(char x, int y, char x1, int y1, char x2, int y2) {
+		Ship s0 = ownGrid.getShipAtCoordinate(new Coordinate(x, y));
+		Ship s1 = ownGrid.getShipAtCoordinate(new Coordinate(x1, y1));
+		Ship s2 = ownGrid.getShipAtCoordinate(new Coordinate(x1, y1));
+		assertEquals(s0, ship);
+		assertEquals(s1, ship);
+		assertEquals(s2, ship);
+
 	}
 
 }
