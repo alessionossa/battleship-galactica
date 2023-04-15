a43fe32565f5af6d99314f9e22a26c793c68a863
Carla <s226573@dtu.dk>
2023-04-15 12:00:12 +0200

    Fixing missing import

--- Battleship/GUI/src/main/java/module-info.java
+++ Battleship/GUI/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.galactica.gui {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.galactica.model;
+    requires javafx.graphics;
 
 
     opens com.galactica.gui to javafx.fxml;
