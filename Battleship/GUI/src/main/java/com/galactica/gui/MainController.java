135da802198015d776a1d0df257001aabeca7bad
Carla <s226573@dtu.dk>
2023-04-19 16:06:08 +0200

    Refactor

--- Battleship/GUI/src/main/java/com/galactica/gui/MainController.java
+++ Battleship/GUI/src/main/java/com/galactica/gui/MainController.java
@@ -17,28 +17,16 @@ public class MainController {
     private boolean gravity;
     private boolean asteroids;
 
+    //TODO Handling button help
+
     @FXML
-    public void switchToSceneSettingsMultiplayer(ActionEvent event) throws IOException {
+    public void switchToSceneSettings(ActionEvent event) throws IOException {
 
         Parent root = FXMLLoader.load(getClass().getResource("settings-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
-        //TODO: Send info to model
-    }
-
-    @FXML
-    public void switchToSceneSettingsSingleplayer(ActionEvent event) throws IOException {
-
-        Parent root = FXMLLoader.load(getClass().getResource("settings-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-        //TODO: Send info to model
     }
 
     @FXML
