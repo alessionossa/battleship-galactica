627097298bf2736cc465fa4ecce6b23e2eb60b64
Carla <s226573@dtu.dk>
2023-04-19 14:43:35 +0200

    Create main scene

--- Battleship/GUI/src/main/java/com/galactica/gui/Main.java
+++ Battleship/GUI/src/main/java/com/galactica/gui/Main.java
@@ -2,7 +2,7 @@ package com.galactica.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,14 +13,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+        Scene scene = new Scene(root);
         stage.setTitle("Battleship");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
---
+++ Battleship/GUI/src/main/java/com/galactica/gui/MainController.java
@@ -0,0 +1,28 @@
+package com.galactica.gui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    protected void onMultiplayerButtonClick() {
+        //Switch scene
+
+        //TO DO: Send info to model
+        
+    }
+    @FXML
+    protected void onSinglePlayerButtonClick() {
+        //Switch scene
+        
+        //TO DO: Send info to model
+       
+    }
+}
