e21f42f84d0e995759f46db24afafe24d4b685c3
Carla <s226573@dtu.dk>
2023-04-26 16:11:03 +0200

    set up game controller

--- Battleship/GUI/src/main/java/com/galactica/gui/GameController.java
+++ Battleship/GUI/src/main/java/com/galactica/gui/GameController.java
@@ -3,13 +3,18 @@ package com.galactica.gui;
 import javafx.scene.image.ImageView;
 import javafx.fxml.FXML;
 import javafx.scene.layout.*;
+import java.util.HashMap;
+
 import com.galactica.model.Tile;
+import com.galactica.model.Ship;
 
 public class GameController {
 
     private int gridSize;
-    private int player1;
-    private int player2;
+    private int playerTurn;
+    private boolean singlePlayer;
+    private boolean asteroids;
+    private boolean gravity;
 
     @FXML
     private BorderPane borderPane;
@@ -24,4 +29,27 @@ public class GameController {
     private ImageView backgroundImageView;
 
     private Tile selectedTile;
+
+    private final HashMap<Ship, ImageView> shipImages = new HashMap<>();
+
+    private final HashMap<Integer, ImageView> gridImages = new HashMap<>();
+
+    public GameController(int gridSize, boolean singlePlayer, boolean asteroids, boolean gravity) {
+        this.gridSize = gridSize;
+        this.singlePlayer = singlePlayer;
+        this.asteroids = asteroids;
+        this.gravity = gravity;
+    }
+
+    public void display() {
+    }
+
+    private void createPlayerGrid() {
+
+    }
+
+    private void createOpponentGrid() {
+
+    }
+
 }
