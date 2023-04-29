71aa8d61ec0d2bac87470bd988b354c4fc754b2e
Carla <s226573@dtu.dk>
2023-04-29 11:33:37 +0200

    Trying to implement a grid :)

--- Battleship/GUI/src/main/java/com/galactica/gui/GameController.java
+++ Battleship/GUI/src/main/java/com/galactica/gui/GameController.java
@@ -6,11 +6,18 @@ import javafx.scene.layout.*;
 import java.util.HashMap;
 import javafx.scene.control.Label;
 import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.stage.Stage;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
 
 import com.galactica.model.Tile;
 import com.galactica.model.Ship;
 
 public class GameController {
+
+    private Stage stage;
+    private Scene scene;
 
     private int gridSize;
     private int playerTurn;
@@ -34,16 +41,45 @@ public class GameController {
 
     private final HashMap<Ship, ImageView> shipImages = new HashMap<>();
 
-    private final HashMap<Integer, ImageView> gridImages = new HashMap<>();
-
     public GameController(int gridSize, boolean singlePlayer, boolean asteroids, boolean gravity) {
-        this.gridSize = gridSize;
-        this.singlePlayer = singlePlayer;
-        this.asteroids = asteroids;
-        this.gravity = gravity;
+        /*
+         * this.gridSize = gridSize;
+         * this.singlePlayer = singlePlayer;
+         * this.asteroids = asteroids;
+         * this.gravity = gravity;
+         */
+
+        this.gridSize = 15;
+        this.singlePlayer = false;
+        this.asteroids = false;
+        this.gravity = false;
+
     }
 
-    public void display() {
+    public void initialize() {
+        // TODO: Set up the player mode, asteroids and gravity if needed here
+
+        backgroundImageView.fitWidthProperty().bind(grid.widthProperty());
+        backgroundImageView.fitHeightProperty().bind(grid.heightProperty());
+
+        grid.getStyleClass().add("grid");
+
+        createPlayerGrid();
+        createOpponentGrid();
+
+        // Bind the column and row constraints to maintain square tiles
+        NumberBinding tileSize = Bindings.min(borderPane.widthProperty().divide(gridSize + 2),
+                borderPane.heightProperty().divide(gridSize + 2));
+        tileSize.addListener((obs, oldSize, newSize) -> {
+            for (ColumnConstraints column : grid.getColumnConstraints()) {
+                column.setPrefWidth(newSize.doubleValue());
+                column.setMaxWidth(newSize.doubleValue());
+            }
+            for (RowConstraints row : grid.getRowConstraints()) {
+                row.setPrefHeight(newSize.doubleValue());
+                row.setMaxHeight(newSize.doubleValue());
+            }
+        });
     }
 
     private void createPlayerGrid() {
