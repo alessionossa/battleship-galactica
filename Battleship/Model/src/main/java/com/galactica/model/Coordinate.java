4a1feaff24f37b335000048a77b4a187e19b11fd
Emilie <126808952+s215162@users.noreply.github.com>
2023-04-30 17:48:28 +0200

    Commented

--- Battleship/Model/src/main/java/com/galactica/model/Coordinate.java
+++ Battleship/Model/src/main/java/com/galactica/model/Coordinate.java
@@ -4,28 +4,34 @@ import java.math.BigDecimal;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-public class Coordinate { // Send new coordinates to player
+// Send new coordinate to player
+public class Coordinate {
     private char x;
     private int y;
     private static int maxValue;
 
+    // Set the maximum value for the coordinate axis
     public static final void setMaxValue(int maxValue) {
         Coordinate.maxValue = maxValue;
     }
 
+    // Constructor to initialize the Coordinate with x and y values
     public Coordinate(char x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    // Getter for the x value
     public char getX() {
         return x;
     }
 
+    // Getter for the y value
     public int getY() {
         return y;
     }
 
+    // Move the coordinate up by the specified distance
     public Coordinate up(int distance) throws OutOfBoundsException {
         if (y > 0)
             return new Coordinate(x, y - distance);
@@ -33,6 +39,7 @@ public class Coordinate { // Send new coordinates to player
             throw new OutOfBoundsException("Cannot go up");
     }
 
+    // Move the coordinate down by the specified distance
     public Coordinate down(int distance) throws OutOfBoundsException {
         if (y < (maxValue - 1))
             return new Coordinate(x, y + distance);
@@ -40,6 +47,7 @@ public class Coordinate { // Send new coordinates to player
             throw new OutOfBoundsException("Cannot go down");
     }
 
+    // Move the coordinate left by the specified distance
     public Coordinate left(int distance) throws OutOfBoundsException {
         if (x > 'a')
             return new Coordinate((char) (x - distance), y);
@@ -47,6 +55,7 @@ public class Coordinate { // Send new coordinates to player
             throw new OutOfBoundsException("Cannot go left");
     }
 
+    // Move the coordinate right by the specified distance
     public Coordinate right(int distance) throws OutOfBoundsException {
         if (x < (char) ('a' + (maxValue - 1)))
             return new Coordinate((char) (x + distance), y);
@@ -54,6 +63,7 @@ public class Coordinate { // Send new coordinates to player
             throw new OutOfBoundsException("Cannot go right");
     }
 
+    // Calculate the distance between this Coordinate and the given Coordinate
     public int distance(Coordinate coordinate) {
         return Math.abs(x - coordinate.x) + Math.abs(y - coordinate.y);
     }
@@ -79,6 +89,7 @@ public class Coordinate { // Send new coordinates to player
                 toHashCode();
     }
 
+    // Method for converting the Coordinate to a JsonObject
     public JsonObject toJsonObject() {
         JsonObject jo = new JsonObject();
         jo.put("x", String.valueOf(x));
@@ -87,6 +98,7 @@ public class Coordinate { // Send new coordinates to player
         return jo;
     }
 
+    // Method for creating a Coordinate object from a JsonObject
     public static Coordinate fromJsonObject(JsonObject jo) {
         char x = ((String) jo.get("x")).charAt(0);
         int y = ((BigDecimal) jo.get("y")).intValue();
