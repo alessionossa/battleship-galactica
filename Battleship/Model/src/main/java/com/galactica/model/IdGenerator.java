c63239b9df480b174658663780d5a88cd0f970c5
Emilie <126808952+s215162@users.noreply.github.com>
2023-04-30 17:25:00 +0200

    Commented

--- Battleship/Model/src/main/java/com/galactica/model/IdGenerator.java
+++ Battleship/Model/src/main/java/com/galactica/model/IdGenerator.java
@@ -1,9 +1,12 @@
 package com.galactica.model;
 
+// IdGenerator class for generating unique integer IDs
 public class IdGenerator {
 
+    // A static integer variable to store the current ID value
     private static int generator = 1;
 
+    // Public static method to get the next unique ID and increment the generator
     public static int get() {
         return generator++;
     }
