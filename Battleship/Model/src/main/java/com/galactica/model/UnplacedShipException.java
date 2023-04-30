d6ed6dca6f1493dd422b200b2636add6e4261233
Emilie <126808952+s215162@users.noreply.github.com>
2023-04-30 15:24:41 +0200

    Commented

--- Battleship/Model/src/main/java/com/galactica/model/UnplacedShipException.java
+++ Battleship/Model/src/main/java/com/galactica/model/UnplacedShipException.java
@@ -1,7 +1,12 @@
+// Declare the package and class
 package com.galactica.model;
 
+// Declare a custom exception class that extends the built-in Exception class
 public class UnplacedShipException extends Exception {
+    
+    // Declare a constructor that takes a message string as a parameter
     public UnplacedShipException(String message) {
+        // Call the constructor of the parent Exception class and pass the message string to it
         super(message);
     }
 }
